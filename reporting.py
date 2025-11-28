2607ad4ce4444ce8db83c47f96cc2977a80c11d5
Mohammed RHAZI <simorhazi@Mohammeds-MacBook-Air.local>
2025-11-27 21:36:54 -0600

    add functions

--- reporting.py
+++ reporting.py
@@ -5,3 +5,4 @@ from data_loader import load_data_pandas, load_data_polars
 from metrics import rolling_metrics_pandas, rolling_metrics_polars
 from parallel import compute_metrics_threading, compute_metrics_multiprocessing, measure_cpu_mem_during
 
+def plot_rolling_metrics(df: pd.DataFrame, window: int=20, )
